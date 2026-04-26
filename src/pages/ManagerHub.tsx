5063e237eae9820ae385d318a4d0eb6e8fde18e4
Eranda Wakista <eranda.wakista@hnbassurance.com>
2026-04-26 11:02:40 +0530

    feat: add ManagerHub page

---
+++ src/pages/ManagerHub.tsx
@@ -0,0 +1,8 @@
+export default function ManagerHub() {
+  return (
+    <div style={{ padding: '24px' }}>
+      <h1>Manager Hub</h1>
+      <p>Manager coaching features coming soon...</p>
+    </div>
+  );
+}
